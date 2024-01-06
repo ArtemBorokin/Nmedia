c2816eda9ff7ca6efb2b374b2e76ed104db22b1e
Artem Borokin <brutalgrin@gmail.com>
2024-01-07 02:17:21 +0300

    Storage

--- app/src/main/java/ru/netology/nmedia/viewmodel/PostViewModel.kt
+++ app/src/main/java/ru/netology/nmedia/viewmodel/PostViewModel.kt
@@ -1,13 +1,11 @@
 package ru.netology.nmedia.viewmodel
 
-import android.view.View
-import androidx.constraintlayout.widget.ConstraintLayout
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import ru.netology.nmedia.R
 import ru.netology.nmedia.dto.Post
 import ru.netology.nmedia.repository.PostRepository
-import ru.netology.nmedia.repository.PostRepositoryInMemoryImpl
+import ru.netology.nmedia.repository.PostRepositorySharedPrefsImpl
 
 private val empty = Post(
     id = 0,
@@ -21,37 +19,37 @@ private val empty = Post(
     video = null
 )
 
-class PostViewModel : ViewModel() {
+class PostViewModel(application: Application) : AndroidViewModel(application) {
 
-    private val repository: PostRepository = PostRepositoryInMemoryImpl()
+    private val repository: PostRepository = PostRepositorySharedPrefsImpl(application)
     val data = repository.getAll()
     val edited = MutableLiveData(empty)
 
     fun likeById(id: Long) = repository.likeById(id)
-    fun sharedById (id: Long) = repository.sharedById(id)
-    fun removeById (id: Long) = repository.removeById(id)
+    fun sharedById(id: Long) = repository.sharedById(id)
+    fun removeById(id: Long) = repository.removeById(id)
 
 
     fun edit(post: Post) {
         edited.value = post
     }
 
-    fun cancelEdit () {
+    fun cancelEdit() {
         edited.value = empty
     }
 
     fun changeContentAndSave(content: String) {
-            val text = content.trim()
-            if (edited.value?.content == text) {
-                return
-            }
+        val text = content.trim()
+        if (edited.value?.content == text) {
+            return
+        }
         edited.value.let {
             if (it != null) {
                 repository.save(it.copy(content = text))
             }
         }
         edited.value = empty
-            }
+    }
 
 }
 
